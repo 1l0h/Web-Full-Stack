d2cf05227a963177ff8b97dafb5dcd34572b182b
1l0h <ilobatomartin22@gmail.com>
2025-12-03 12:27:30 +0100

    cambios morrocotudos
    mi trabajo con los cambios morrocotudos

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 // backend/server.js
-//1º DAM JRM (c) 2025/2026
 const http = require('http');
 const port = 3000;
-//Abre tu navegador en http://localhost:3000.
+
 const requestHandler = (request, response) => {
-  response.end('Este es mi backend en el puerto 3000, es un archivo .js, y Sandoval es un tocacojones');
+  // 1. Establecer el tipo de contenido como JSON
+  response.setHeader('Content-Type', 'application/json');
+
+  // 2. Establecer cabeceras para permitir llamadas desde el frontend (CORS)
+  response.setHeader('Access-Control-Allow-Origin', '*'); 
+
+  // 3. Devolver un objeto JSON
+  response.end(JSON.stringify({ 
+      status: 'ok', 
+      message: 'El BAKCEND FUNCIONA! SANDOVAL ROJO!',
+      timestamp: new Date().toISOString()
+  }));
 };
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('Algo salió mal', err);
+    return console.log('Algo salió mal y sandoval es un rojazo de época', err);
   }
   console.log(`Servidor Node.js escuchando en el puerto ${port}`);
 });
